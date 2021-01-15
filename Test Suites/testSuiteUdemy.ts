f4ffdada6fc69c4bacd9c2f3aabc4162ac701364
agus <agus@10.81.234.103>
2021-01-15 11:20:05 +0700

    testBelajarKatalon

---
+++ Test Suites/testSuiteUdemy.ts
@@ -0,0 +1,26 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>testSuiteUdemy</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>0373194f-b853-4b06-9d0a-92ddd6a995b0</testSuiteGuid>
+   <testCaseLink>
+      <guid>9fca73d8-e7c1-4f2d-aa6f-5927a7549ab7</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TestUdemyRecord</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>5e6b7953-a1df-4b60-a920-2bec1552a30a</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/UdemyTest</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
